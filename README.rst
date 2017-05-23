f8798b4d2e8d847dae2e792235e579fbe8810be5
Tarek Ziade <tarek@ziade.org>
2017-05-23 09:26:01 +0200

    described the whole process

--- README.rst
+++ README.rst
@@ -1,19 +1,42 @@
 Dogdriver
 =========
 
+This project aims at collecting performance metrics to build a
+dashboard displaying a trend. The goal is to answer this simple
+question : is this service getting slower or faster over time.
 
-The Dogdriver does the following:
+The Dogdriver is composed of two parts:
 
+- a client that performs a smoe test using Molotov
+- a server that collects metrics from datadog and displays them
+
+Client
+------
+
+When the client runs it does the following:
+
+- query the ServiceBook to get the URL of the molotov test.
 - sends a start event to datadog
 - runs a calibrated smoke+load test using molotov on a stack
 - sends a stop event to datadog
-- queries metrics from datadog between the two events, like elb http counts
-- sends that data to a specialized DB
+- sends the start and stop values to the Dogdriver Server
 
-The Dogdriver can be run continuously to build a high-level
-performance trend per application.
 
-Trends will be simple % with arrows (up/down/same)
+Server
+------
 
+The server does the following:
+
+- exposes an API to receive tests runs. Each run is stored in a queue
+- when a run is older than 10 minutes, the server queries Datadog to build metrics
+- Metrics are stored as JSON files on the server, grouped by project names
+- a metrics displaying the performance trend for a specific project is updated
+- a simple JS dashboard display those all those metrics
+
+Integration
+-----------
+
+The Dogdriver client is added to a Jenkins pipeline, so it can be
+run continuously to build a high-level performance trend per application.
 
 
--- dogdrive.py
+++ dogdrive.py
@@ -1,4 +1,4 @@
-# Configure the module according to your needs
+import json
 import sys
 import os
 import time
@@ -19,40 +19,42 @@ text = "We're doing a Molotov test on kintowe"
 _tags = ['version:1', 'app:kintowe']
 
 
-def start(test_name):
+def _start(test_name):
     tags = list(_tags)
     tags.append('step:start')
     tags.append('test:%s' % test_name)
     return api.Event.create(title=title, text=text, tags=tags)
 
 
-def stop(test_name):
+def _stop(test_name):
     tags = list(_tags)
     tags.append('step:stop')
     tags.append('test:%s' % test_name)
     return api.Event.create(title='Molotov-Stop', text=text, tags=tags)
 
 
-# the test
-start_event = start("My test")
-try:
-    # run the molotov test against the stack
-    args = ['moloslave', 'https://github.com/tarekziade/kinto-loadtests',
-            'dogdriver']
-
-    old = list(sys.argv)
-    sys.argv = args
+def run_test(name="My Test"):
+    # the test
+    start_event = _start(name)
     try:
-        moloslave()
-    except SystemExit:
-        pass
-    finally:
-        sys.argv = old
-finally:
-    stop_event = stop("My Test")
+        # run the molotov test against the stack
+        args = ['moloslave', 'https://github.com/tarekziade/kinto-loadtests',
+                'dogdriver']
 
-start = start_event['event']['date_happened']
-end = stop_event['event']['date_happened']
+        old = list(sys.argv)
+        sys.argv = args
+        try:
+            moloslave()
+        except SystemExit:
+            pass
+        finally:
+            sys.argv = old
+    finally:
+        stop_event = _stop(name)
+
+    start = start_event['event']['date_happened']
+    end = stop_event['event']['date_happened']
+    return start, end
 
 
 _200 = 'aws.elb.httpcode_backend_2xx{app:kintowe,env:stage}'
@@ -63,20 +65,31 @@ max:system.cpu.system{app:kintowe,env:stage,type:web} by {host} +
 max:system.cpu.stolen{app:kintowe,env:stage,type:web} by {host}"""
 
 
-# lets wait 5 minutes
-print('zZZzZzz')
-time.sleep(60*5)
+def create_metrics(start, end):
+    # now we want to grab metrics on that time window
+    results = {}
+    results['CPU'] = api.Metric.query(start=start, end=end, query=_CPU)
+    results['RPS'] = api.Metric.query(start=start, end=end, query=_REQ)
+    results['200'] = api.Metric.query(start=start, end=end, query=_200)
+    here = os.path.dirname(__file__)
+    filename = 'dogdrive-%s.json' % str(start)
+    filename = os.path.join(here, filename)
 
-# now we want to grab metrics on that time window
-results = {}
+    with open(filename, 'w') as f:
+        f.write(json.dumps(results))
 
-results['CPU'] = api.Metric.query(start=start, end=end, query=_CPU)
-results['RPS'] = api.Metric.query(start=start, end=end, query=_REQ)
-results['200'] = api.Metric.query(start=start, end=end, query=_200)
+    return filename
 
-filename = 'dogdrive-%s' % str(start)
 
-with open(filenam, 'w') as f:
-    f.write(json.dumps(results))
+def main():
+    start, end = run_test()
+    # lets wait 6 minutes
+    print('zZZzZzz')
+    time.sleep(60*6)
+    import pdb; pdb.set_trace()
+    filename = create_metrics(start, end)
+    print('%s created.' % filename)
 
-print('%s created.' % filename)
+
+if __name__ == '__main__':
+    main()
