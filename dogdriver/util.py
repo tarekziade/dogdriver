eac9cb89b357d01955a05605b1f4fecbfb6f3f92
Tarek Ziade <tarek@ziade.org>
2017-05-31 12:30:03 +0200

    make sure the server can run w/o the datadog creds

--- dogdriver/util.py
+++ dogdriver/util.py
@@ -12,10 +12,11 @@ def init_api():
     global _INIT
     if _INIT:
         return
-    APP_KEY = os.environ['DOG_APP_KEY']
-    API_KEY = os.environ['DOG_API_KEY']
-    initialize(app_key=APP_KEY, api_key=API_KEY)
-    _INIT = True
+    if 'DOG_APP_KEY' in os.environ:
+        APP_KEY = os.environ['DOG_APP_KEY']
+        API_KEY = os.environ['DOG_API_KEY']
+        initialize(app_key=APP_KEY, api_key=API_KEY)
+        _INIT = True
 
 
 init_api()
